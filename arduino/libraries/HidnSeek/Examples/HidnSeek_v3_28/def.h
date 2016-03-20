fab18952dd18352b58129c0f7289530e067201d0
Stephane Driussi <stephane.driussi@hidnseek.fr>
2016-03-20 16:58:04 +0100

    Minor changes
    - Remove unused variables and definition
    - Change type values to proper scale

--- arduino/libraries/HidnSeek/Examples/HidnSeek_v3_28/def.h
+++ arduino/libraries/HidnSeek/Examples/HidnSeek_v3_28/def.h
@@ -1,44 +1,39 @@
 /*  This file is part of HidnSeek.
 
- HidnSeek is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+  HidnSeek is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
- HidnSeek is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+  HidnSeek is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with HidnSeek.  If not, see <http://www.gnu.org/licenses/>.*/
+  You should have received a copy of the GNU General Public License
+  along with HidnSeek.  If not, see <http://www.gnu.org/licenses/>.*/
 
-#define ACCEL_MODE 2
+#define ACCEL_MODE 2        // 2G scale for the accelerometer
 #define ACCEL_TRIG 40
 #define ACCEL_FLAT 10
+#define SPORT_LIMIT 48      // 4h limit sport duration
+#define PERIOD_LOOP 10      // number of minutes between messages, must be > 10
+#define MOTION_MIN_NUMBER 2
+
+// --------------- do not change parameters below this line -------------------
+
+#define PERIOD_COUNT ((PERIOD_LOOP * 15) >> 1)
 
 #define BATT_MIN 3570
 #define BATT_MAX 4200
 
 // EEPROM map
-#define ADDR_TODAY 0
-#define ADDR_SENT 1
-// byte1-31: number of messages sent
-#define ADDR_HOUR 32
-// byte32: number of messages this hour
-#define ADDR_LAST 33
-// byte33-39:cent/year/month/day/hour/min/sec of last message
-// byte40-41:battery calibration
-#define ADDR_CAL_LOW  40
-#define ADDR_CAL_HIGH 41
+#define ADDR_TODAY     0
+#define ADDR_SENT      1 // byte01-31: number of messages sent
+#define ADDR_CAL_LOW  32 // byte32-33: battery calibration
+#define ADDR_CAL_HIGH 33
 
-#define MsgMax     140   // 12 messages can be sent per hour.
-#define MOTION_MIN_NUMBER 2
-#define PMTK_SET_NMEA_OUTPUT_RMCONLY "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*" // 29"
-#define PMTK_SET_NMEA_OUTPUT_RMCGGA  "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*" // 28"
-#define PMTK_SET_NMEA_OUTPUT_RMCGGA5 "$PMTK314,0,1,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*" // 2C"
-#define PMTK_SET_NMEA_OUTPUT_DATA    "$PMTK314,0,3,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*" // 29"
-#define PMTK_SET_NMEA_OUTPUT_ALLDATA "$PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0*" // 28"
+#define PMTK_SET_NMEA_OUTPUT  "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*" // 28"
 
 #define PMTK_AWAKE   "$PMTK010,001*" // 2E"
 #define PMTK_STANDBY "$PMTK161,0*"   // 28"
@@ -47,8 +42,8 @@
 #define PMTK_ENABLE_SBAS "$PMTK313,1*" // 2E"
 #define PMTK_ENABLE_WAAS "$PMTK301,2*" // 2E"
 
-enum {
-  MSG_POSITION = 0, MSG_SPORT = 3, MSG_NO_MOTION = 4, MSG_NO_GPS = 5,
+enum msgs {
+  MSG_POSITION = 0, MSG_OPTION = 3, MSG_NO_MOTION = 4, MSG_NO_GPS = 5,
   MSG_MOTION_ALERT = 6, MSG_WEAK_BAT = 7
 };
 
@@ -59,8 +54,8 @@ enum {
 };
 
 /****************** Pins usage ***************************************/
-#define recPin           0     // PD0 RX Serial from GPS
-//                       1     // PD1 TX Serial to GPS
+#define rxGPS            0     // PD0 RX Serial from GPS
+#define txGPS            1     // PD1 TX Serial to GPS
 #define usbDP            2     // PD2 Shutdown supply after power off
 #define accINT           3     // PB0 Accelerometer Interruption
 #define usbDM            4     // PD4
@@ -89,101 +84,102 @@ enum {
 /*********************************************************************/
 
 /****************** Pins direction ***********************************/
-#define DDRC_MASK (1 << 2) | (1 << 6)
-#define DIGITAL_OUTPUT ((1 << shdPin) | (1 << redLEDpin) | (1 << bluLEDpin) | (1 << rxSigfox))
+#define DDRC_MASK (1 << 2)
+#define DIGITAL_OUTPUT ((1 << shdPin) | (1 << redLEDpin) | (1 << bluLEDpin) | (1 << rxSigfox) | (1 << rstPin))
 /*********************************************************************/
-
-unsigned char buffer[32];
 
 unsigned long fix_age = 0;
 int year = 0;
 byte month, day, hour, minute, second, hundredths = 0;
-byte msgs = 0;
-boolean modemPresent = false;
+
 boolean accelPresent = false;
 boolean baromPresent = false;
 boolean GPSactive = true;
 
 // For automatic airplane mode detection
-boolean highSpeed     = false;
-boolean airPlaneMode  = false;
 boolean airPlaneSpeed = false;
 boolean airPlanePress = false;
 
-unsigned int loopGPS = 0;
-unsigned long startCW = 0;
+uint8_t loopGPS = 0;
 
-#define limitSport 48
-unsigned long debugSport = 0;
+uint8_t limitSport = 0;
 uint8_t forceSport = 0;
-unsigned long loopSport = 0;
 
-unsigned long start = millis();
-unsigned long timer = millis();
-unsigned long fixTime = millis();
+unsigned long start = 0;
 uint8_t today = 0;
 uint8_t MsgCount = 0;
 
 byte accelPosition;
-int detectMotion = 1;
+int8_t detectMotion = 1;
 unsigned int batteryValue;
 byte batteryPercent = 0;
 
-unsigned int alt = 0;
-unsigned int spd = 0;
-unsigned int sat = 0;
-unsigned int syncSat = 0;
-unsigned int noSat = 0;
+uint16_t alt = 0;
+uint16_t spd = 0;
+uint8_t  sat = 0;
+uint8_t  syncSat = 0;
+uint8_t  noSat = 0;
 
 float previous_lat = 0;
 float previous_lon = 0;
 
 // BMP180 measurements
-float Temp;
-unsigned int Press;
+float    Temp = 0;
+uint16_t Press = 0;
 
 // 12 octets = 96 bits payload
 // lat: 32, lon: 32, alt: 13 , spd: 7, bat: 7, mode: 3, cap: 2
 // lat: 32, lon: 32, alt:0-8192m, spd:0-127Km/h, bat:0-100%, mode:0-7, cap: N/E/S/W
 // int is 16 bits, float is 32 bits. All little endian
-typedef struct {
+struct Payload {
   float lat;
   float lon;
   uint32_t cpx;
-}
-Payload;
+};
 
 Payload p;
 
 /* Hardware and Software Changes Log
 
- 2016-01-17
- - Fix Temp and Press not updated before each non GPS message
+  2016-03-20
+  - Remove unused variables
+  - Change calibration algorithm in battery sketch. After reboot, always need a full charge cycle
+  - Change detectmotion algorithm, add motion when GPS speed > 15Km/h
 
- 2016-01-14
- - Use new barometer library from seeedstudio and save 1Kb of flash program space
+  2016-02-27
+  - Direct port manipulation code
 
- 2015-10-08
- - Fix standby current excessive consumption from 2mA to 0,2mA (D5)
+  2016-02-04
+  - Fix loop duration during GPS process
+  - New speed algorithm
 
- 2015-09-10
- - Fix battery calibration EEPROM address not correct
+  2016-01-17
+  - Fix Temp and Press not updated before each non GPS message
 
- 2015-08-26
- - Add Temperature and pressure measuremements from BMP180
- - Change Bootloader to always enter in DFU mode and exit with TIMEOUT
- - Add HidnSeek dance to shutdown the device
+  2016-01-14
+  - Use new barometer library from seeedstudio and save 1Kb of flash program space
 
- 2015-08-17
- - Add Eeprom datalog
+  2015-10-08
+  - Fix standby current excessive consumption from 2mA to 0,2mA (D5)
 
- 2015-08-15
- - New Hardware release 3.24
- - Fix: Avoid GPS go to sleep when green LED is high
+  2015-09-10
+  - Fix battery calibration EEPROM address not correct
 
- 2015-07-31
- - Fix initial start sequence when battery is low
+  2015-08-26
+  - Add Temperature and pressure measuremements from BMP180
+  - Change Bootloader to always enter in DFU mode and exit with TIMEOUT
+  - Add HidnSeek dance to shutdown the device
 
- */
+  2015-08-17
+  - Add Eeprom datalog
+
+  2015-08-15
+  - New Hardware release 3.24
+  - Fix: Avoid GPS go to sleep when green LED is high
+
+  2015-07-31
+  - Fix initial start sequence when battery is low
+
+*/
 
 
