01c61438fc8ab46ce60e6652bedc352ded225830
Stephane Driussi <stephane.driussi@hidnseek.fr>
2016-03-20 16:49:02 +0100

    Minor change
    - Comments formatted
    - New calibration algorithm, reset calibration value after each restart
    - Remove 12mV to compensate battery internal resistance during charge

--- arduino/libraries/HidnSeek/Examples/HidnSeek_v3_28/battery.ino
+++ arduino/libraries/HidnSeek/Examples/HidnSeek_v3_28/battery.ino
@@ -1,19 +1,17 @@
 /*  This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
- HidnSeek by StephaneD 20150313 Not for commercial use              */
-
-#include <EEPROM.h> //Needed to access the eeprom read write functions
+  HidnSeek by StephaneD 20150313 Not for commercial use              */
 
 unsigned int sensorMax;
 
@@ -21,12 +19,9 @@ void initSense() {
   byte lowByte = EEPROM.read(ADDR_CAL_LOW);
   byte highByte = EEPROM.read(ADDR_CAL_HIGH);
   sensorMax = ((lowByte << 0) & 0xFF) + ((highByte << 8) & 0xFF00);
-  serialString(PSTR("SensorMax "));
-  if (sensorMax < 980 || sensorMax > 1023) {
-    serialString(PSTR("default "));
-    sensorMax = 980;
-  }
-  Serial.println(sensorMax);
+  serialString(PSTR("SensorMax: "));
+  Serial.print(sensorMax);
+  sensorMax = 980;
 }
 
 unsigned int calibrate(unsigned int sensorValue) {
@@ -72,7 +67,8 @@ bool batterySense() {
   }
   batteryValue = batteryValue >> 1;
 
-  if (batteryValue > sensorMax) sensorMax = calibrate(batteryValue);
+  unsigned int batteryComp = batteryValue - 4; // remove 16mV
+  if (batteryComp > sensorMax) sensorMax = calibrate(batteryComp);
   unsigned int bat = map(batteryValue, 0, sensorMax, 0, BATT_MAX); // represent the battery voltage
   batteryPercent = map(bat, min(bat, BATT_MIN), max(bat, BATT_MAX), 0, 100);
   digitalWrite(rstPin, (forceSport || GPSactive) ? HIGH : LOW);
