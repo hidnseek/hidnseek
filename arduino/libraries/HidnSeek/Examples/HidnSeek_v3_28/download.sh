18034b04af29cfa2d82f5fca5009403843b03778
Stephane Driussi <stephane.driussi@hidnseek.fr>
2016-05-19 21:37:37 +0200

    create libraries zip

--- arduino/libraries/HidnSeek/Examples/HidnSeek_v3_28/download.sh
+++ arduino/libraries/HidnSeek/Examples/HidnSeek_v3_28/download.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 avrdude -p atmega328p -c usbasp -P usb -U flash:w:HidnSeek_v3_28.hex:i
+while [ $? != 0 ]
+do
+  sleep 5
+  avrdude -p atmega328p -c usbasp -P usb -U flash:w:HidnSeek_v3_28.hex:i
+done
