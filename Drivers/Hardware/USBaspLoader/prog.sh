39212f1989d5b6300e776a87ce19e1770a256f24
HidnSeek <stephane.driussi@gmail.com>
2016-08-21 14:09:34 +0200

    Support for external hex

--- Drivers/Hardware/USBaspLoader/prog.sh
+++ Drivers/Hardware/USBaspLoader/prog.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-avrdude -p atmega328p -c usbasp -P usb -e -Ulock:w:0x3F:m -Uefuse:w:0x05:m -Uhfuse:w:0xd8:m -Ulfuse:w:0xff:m
-avrdude -p atmega328p -c usbasp -P usb -U flash:w:main.hex:i -Ulock:w:0x0F:m
-
+FILE=$1
+if [ ! -f $FILE ] || [ $# -eq 0 ]; then
+  FILE="main.hex"
+  avrdude -p atmega328p -c usbasp -P usb -e -Ulock:w:0x3F:m -Uefuse:w:0x05:m -Uhfuse:w:0xd8:m -Ulfuse:w:0xff:m
+  avrdude -p atmega328p -c usbasp -P usb -U flash:w:$FILE:i -Ulock:w:0x0F:m
+else
+  avrdude -p atmega328p -c usbasp -P usb -U flash:w:$FILE:i
+fi
